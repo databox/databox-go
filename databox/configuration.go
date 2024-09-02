097bba02312a6933fa0f5b67eb91d959ea4235d6
slavkor <30171206+slavkor@users.noreply.github.com>
2024-09-02 09:14:56 +0000

    chore: release data-link SDK

--- databox/configuration.go
+++ databox/configuration.go
@@ -89,7 +89,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/v0.3.6/go",
+		UserAgent:        "OpenAPI-Generator/v0.3.7/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
